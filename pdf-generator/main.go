deb44da8b3a2ced11f2f3a0c61e2420fd4736578
Vivek Mishra <v.mishra@f5.com>
2024-07-08 10:42:38 +0530

    pdf invoice summary

--- pdf-generator/main.go
+++ pdf-generator/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	bannerHt = 95.0
+	bannerHt = 94.0
 	xIndent  = 40.0
 )
 
@@ -37,29 +37,67 @@ func main() {
 	pdf.SetFont("Arial", "B", 40)
 	pdf.SetTextColor(255, 255, 255)
 	_, lineHt := pdf.GetFontSize()
-	pdf.Text(xIndent, bannerHt-(bannerHt-lineHt)/2.0, "INVOICE")
+	pdf.Text(xIndent, bannerHt-(bannerHt/2.0)+lineHt/3.1, "INVOICE")
 
 	// Banner - Phone, email, domain
 	pdf.SetFont("Arial", "", 12)
 	pdf.SetTextColor(255, 255, 255)
 	_, lineHt = pdf.GetFontSize()
-	pdf.MoveTo(w-xIndent-2.0*124.0, bannerHt-(lineHt*1.5*3.0)/2.0)
-	pdf.MultiCell(124.0, lineHt*1.5, "(123) 456-7890\n vivek@techtraversal.com\ntechtraversal.com", gofpdf.BorderNone, gofpdf.AlignRight, false)
+	pdf.MoveTo(w-xIndent-2.0*124.0, (bannerHt-(lineHt*1.5*3.0))/2.0)
+	pdf.MultiCell(124.0, lineHt*1.5, "(123) 456-7890\n vivek@techt.com\ntechtraversal.com", gofpdf.BorderNone, gofpdf.AlignRight, false)
 
 	// Banner - Address
 	pdf.SetFont("Arial", "", 12)
 	pdf.SetTextColor(255, 255, 255)
 	_, lineHt = pdf.GetFontSize()
-	pdf.MoveTo(w-xIndent-124.0, bannerHt-(lineHt*1.5*3.0)/2.0)
+	pdf.MoveTo(w-xIndent-124.0, (bannerHt-(lineHt*1.5*3.0))/2.0)
 	pdf.MultiCell(124.0, lineHt*1.5, "123 Fake Street\n Some Town, PA, IN\nPin-12345", gofpdf.BorderNone, gofpdf.AlignRight, false)
 
+	_, sy := summaryBlock(pdf, xIndent, bannerHt+lineHt*2.0, "Billed To", "Client Name", "City, State, Country", "Postal Code")
+	summaryBlock(pdf, xIndent*2.0+lineHt*12.5, bannerHt+lineHt*2.0, "Invoice Number", "00000000123")
+	summaryBlock(pdf, xIndent*2.0+lineHt*12.5, bannerHt+lineHt*6.25, "Date of Issue", "05/29/2018")
+	// Summary: Invoice Total
+	x, y := w-xIndent-124.0, bannerHt+lineHt*2.25
+	pdf.MoveTo(x, y)
+	pdf.SetFont("times", "", 14)
+	pdf.SetTextColor(180, 180, 180)
+	pdf.CellFormat(124.0, lineHt, "Invoice Total", gofpdf.BorderNone, gofpdf.LineBreakNone, gofpdf.AlignRight, false, 0, "")
+	y += lineHt * 1.5
+	pdf.MoveTo(x, y)
+	pdf.SetFont("times", "", 48)
+	alpha := 58
+	_, lineHt = pdf.GetFontSize()
+	pdf.SetTextColor(72+alpha, 42+alpha, 55+alpha)
+	totalUSD := "$1234.50"
+	pdf.CellFormat(124.0, lineHt, totalUSD, gofpdf.BorderNone, gofpdf.LineBreakNone, gofpdf.AlignRight, false, 0, "")
+	x, y = x-2.0, y+lineHt*1.25
+
+	if sy > y {
+		y = sy
+	}
+	x, y = xIndent-20, y+30.0
+	pdf.Rect(x, y, w-(xIndent*2.0)+40.0, 3.0, "F")
 	// Grid
 	drawGrid(pdf)
-
 	err := pdf.OutputFileAndClose("hello.pdf")
 	if err != nil {
 		panic(err)
 	}
+}
+
+func summaryBlock(pdf *gofpdf.Fpdf, x, y float64, title string, data ...string) (float64, float64) {
+	pdf.SetFont("times", "", 14)
+	pdf.SetTextColor(180, 180, 180)
+	_, lineHt := pdf.GetFontSize()
+	y += lineHt
+	pdf.Text(x, y, title)
+	y += lineHt * 0.25
+	pdf.SetTextColor(50, 50, 50)
+	for _, str := range data {
+		y += lineHt * 1.25
+		pdf.Text(x, y, str)
+	}
+	return x, y
 }
 
 func drawGrid(pdf *gofpdf.Fpdf) {
